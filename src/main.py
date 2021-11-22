d478b8b4648608116259e79acb323b2c8cd6beb7
vedat <vedat.botuk@gmail.com>
2021-11-22 18:55:51 +0100

    unix_time

--- src/main.py
+++ src/main.py
@@ -66,7 +66,7 @@ def main():
 
             data_master['Devices'][device_name] = get_data
 
-            data_master['Devices'][device_name]['Son Reset Tarihi'] = unix_time_to_date(reset_time)
+            # data_master['Devices'][device_name]['Son Reset Tarihi'] = unix_time_to_date(reset_time)
             data_master['Devices'][device_name]['Çalışma süresi'] = unix_time_to_hhmm(run_time)
             data_master['Devices'][device_name]['Çalışma hızı'] = str(round(speed * 60, 1)) + ' düğüm/dakika'
             data_master['Devices'][device_name]['Tahmini kalan süre'] = unix_time_to_hhmm(remainder_time)
